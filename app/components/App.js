2894591fbff2e40a0dabbc5ddd4d8fe7ea7ad97b
roscolil <ross.lillis@gmail.com>
2018-09-04 20:23:40 +1000

    removed modal, added div to display details

--- app/components/App.js
+++ app/components/App.js
@@ -1,41 +1,14 @@
 import React from 'react'
-import Modal from 'react-modal';
 
 const API = "http://dnu5embx6omws.cloudfront.net/venues/weather.json";
-const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
-  }
-};
-
-Modal.setAppElement('#app')
 
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.inputChange = this.inputChange.bind(this);
     this.sortAlpha = this.sortAlpha.bind(this);
     this.sortTemp = this.sortTemp.bind(this);
     this.sortLastUpdated = this.sortLastUpdated.bind(this);
-
-    // this.state = {
-    //   cuisine: '',
-    //   nearbyRestaurants: [],
-    //   operation: '',
-    //   noOfResults: 0,
-    //   headers: {
-    //     headers: {'user-key': '1e35415e3b9627ff3ad8ace342afde9c'}
-    //   },
-    //   resultOffset: 0,
-    //   distance: 500 ,
-    //   sort: 'real_distance'
-    // }
 
     this.state = {
       data: null,
@@ -45,42 +18,25 @@ export default class App extends React.Component {
       country: "",
       weatherCond: ""
     };
-
-    this.state = {
-      modalIsOpen: true
-    };
   }
 
-
-  openModal() {
-    this.setState({
-      modalIsOpen: true
-    });    
-  }
-
-  // inputChange(e) {
+  // sortAlpha() {
   //   this.setState({
-  //     cuisine: e.target.value
-  //   })
+  //     name: //ascending in alphabetical order
+  //   });
   // }
-
-  sortAlpha() {
-    this.setState({
-      name: //ascending in alphabetical order
-    });
-  }
-
-  sortTemp() {
-    this.setState({
-      temp: //descending in temp
-    });
-  }
-
-  sortLastUpdated() {
-    this.setState({
-      lastUpdated: //ascending in time order
-    });
-  }
+  //
+  // sortTemp() {
+  //   this.setState({
+  //     temp: //descending in temp
+  //   });
+  // }
+  //
+  // sortLastUpdated() {
+  //   this.setState({
+  //     lastUpdated: //ascending in time order
+  //   });
+  // }
 
   componentDidMount() {
     fetch(API)
@@ -94,46 +50,6 @@ export default class App extends React.Component {
       }));
   }
 
-
-    // navigator.geolocation.getCurrentPosition((position) => {
-    //   let lat = position.coords.latitude
-    //   let lon = position.coords.longitude
-    //   let header = this.state.headers
-    //   let url =  `https://developers.zomato.com/api/v2.1/search?start=${this.state.resultOffset}&count=10&lat=${lat}&lon=${lon}&radius=${this.state.distance}&q=${this.state.cusine}&sort=${this.state.sort}`;
-    //
-    //   this.setState({
-    //     operation: 'Searching for fodder...'
-    //   })
-    //
-    //   axios.get(url, header)
-    //     .then(res => {
-    //       this.setState({
-    //         operation: 'Here are your results!',
-    //         nearbyRestaurants: res.data.restaurants,
-    //         resultOffset: this.state.resultOffset + 10,
-    //         noOfResults: res.data.results_found
-    //       })
-    //     })
-    // })
-
-
-  distanceChange(e) {
-    this.setState({
-      distance: e.target.value
-    })
-  }
-
-  sortChange(e) {
-    this.setState({
-      sort: e.target.value
-    })
-  }
-
-  topScroll() {
-    document.body.scrollTop = 0
-    document.documentElement.scrollTop = 0
-  }
-
   render() {
     const name = this.state.name
     const temp = this.state.temp
@@ -142,88 +58,33 @@ export default class App extends React.Component {
     const weatherCond = this.state.weatherCond
 
     return (
-
-      <div>
-        <Modal
-          isOpen={this.state.modalIsOpen}
-          style={customStyles}
-        >
-
+        <div>
           <header>
             <div className="header__div">
               <h1>Weather App</h1>
-              <button className="search__btn" onClick={this.clickSearch}>A-Z</button>
-              <button className="search__btn" onClick={this.clickSearch}>Temperature</button>
-              <button className="search__btn" onClick={this.clickSearch}>Last Updated</button>
+              <button className="search__btn" onClick={this.clickSelection}>A-Z</button>
+              <button className="search__btn" onClick={this.clickSelection}>Temperature</button>
+              <button className="search__btn" onClick={this.clickSelection}>Last Updated</button>
             </div>
           </header>
           <main>
-            <ul>
-              <li>{name}</li>
-              <li>{temp}</li>
-              <li>{lastUpdated}</li>
-              <li>{country}</li>
-              <li>{weatherCond}</li>
-            </ul>
-
+            { results.map(function(resultObj, index) {
+              let item = resultObj.restaurant
+              return (
+                <div className="row" key={index}>
+                  <div className="details">
+                    <p>Name: <span>{item.name}</span></p>
+                    <p>Country: <span>{item.country}</span></p>
+                    <p>Temp: <span>{item.temp}</span></p>
+                    <p>Weather Conditions: <span>{item.weatherCond}</span></p>
+                    <p>Last Updated: <span>{item.lastUpdated}</span></p>
+                  </div>
+                </div>
+                )
+            })
+          }
           </main>
-            <div>
-
-            </div>
-        </Modal>
-      </div>
-
-      // <input type="text" placeholder="Cuisine" className="cuisine__field" value={cuisineInput} onChange={this.inputChange}></input>
-      // <div className="select__boxes">
-      //   <div className="distance__box">
-      //     <label>A-Z</label>
-      //     <select value={this.state.distance} onChange={this.distanceChange}>
-      //       <option value="500">500m</option>
-      //       <option value="1000">1km</option>
-      //       <option value="5000">5km</option>
-      //       <option value="10000">10km</option>
-      //       <option value="20000">20km</option>
-      //     </select>
-      //   </div>
-      //   <div className="sort__box">
-      //     <label>Temperature</label>
-      //     <select value={this.state.sort} onChange={this.sortChange}>
-      //       <option value="real_distance">Distance</option>
-      //       <option value="rating">Rating</option>
-      //       <option value="cost">Cost</option>
-      //     </select>
-      //   </div>
-      // </div>
-      // <p className="progress_msg">{this.state.operation}</p>
-      // <button className="search__btn" onClick={this.clickSearch}>Search</button>
-        //
-        // <main>
-        //     <div className="search__results">
-        //       <p className="number__of__results" style={{
-        //           visibility: this.state.noOfResults === 0 ? 'hidden' : 'visible'
-        //         }}>There are <span>{this.state.noOfResults}</span> total matches</p>
-        //       { results.map(function(resultObj, index) {
-        //         let item = resultObj.restaurant
-        //         return (
-        //           <div className="row" key={index}>
-        //             <div className="image">
-        //               <img src={item.thumb} />
-        //             </div>
-        //             <div className="details">
-        //               <p>Name: <span>{item.name}</span></p>
-        //               <p>Cuisine: <span>{item.cuisines}</span></p>
-        //               <p>Rating: <span>{item.user_rating.aggregate_rating}</span></p>
-        //               <p>Votes: <span>{item.user_rating.votes}</span></p>
-        //               <p>Locality: <span>{item.location.locality}</span></p>
-        //               <p>Address: <span>{item.location.address}</span></p>
-        //             </div>
-        //           </div>
-        //           )
-        //       })
-        //     }
-        //     </div>
-        // </main>
-
+        </div>
     )
   }
 }
